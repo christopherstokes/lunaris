1bc32263796c49c412e8949e0e49a1abb392bc85
Christopher Stokes <christopherstokes@gmail.com>
2019-08-23 20:59:46 -0700

    add bullets

--- game.lua
+++ game.lua
@@ -1,6 +1,7 @@
 local t = 0
 local s_wid = 240
 local s_hei = 136
+local horizon = (s_hei - 48) / 2
 local Entity
 do
   local _class_0
@@ -8,6 +9,12 @@ do
   _base_0.__index = _base_0
   _class_0 = setmetatable({
     __init = function(self, x, y, max_dx, max_dy, acc)
+      if max_dx == nil then
+        max_dx = 3
+      end
+      if max_dy == nil then
+        max_dy = 2
+      end
       self.x = x
       self.y = y
       self.dx = 0
@@ -15,6 +22,7 @@ do
       self.acc = acc
       self.max_dx = max_dx
       self.max_dy = max_dy
+      self.alive = true
       self.turn_l = false
       self.turn_r = false
     end,
@@ -31,8 +39,89 @@ do
   _base_0.__class = _class_0
   Entity = _class_0
 end
+local Bullet
+do
+  local _class_0
+  local _parent_0 = Entity
+  local _base_0 = {
+    upd = function(self)
+      trace(self.dy)
+      if math.abs(self.dy) < math.abs(self.max_dy) then
+        if self.dy < 0 then
+          self.dy = self.dy - self.acc
+        end
+        if self.dy > 0 then
+          self.dy = self.dy + self.acc
+        end
+      end
+      self.y = self.y + self.dy
+      if self.y < horizon + 40 then
+        self.r = 1
+      end
+      if self.y < horizon + 16 then
+        self.r = 0
+      end
+      if self.y < horizon - 4 or self.y > s_hei then
+        self.alive = false
+      else
+        if self.r == 0 then
+          return pix(self.x, self.y, self.col)
+        else
+          return circ(self.x, self.y, self.r, self.col)
+        end
+      end
+    end
+  }
+  _base_0.__index = _base_0
+  setmetatable(_base_0, _parent_0.__base)
+  _class_0 = setmetatable({
+    __init = function(self, x, y, acc, r, col, dy, max_dx, max_dy)
+      if col == nil then
+        col = 3
+      end
+      if dy == nil then
+        dy = -1
+      end
+      if max_dx == nil then
+        max_dx = 0
+      end
+      if max_dy == nil then
+        max_dy = 2
+      end
+      _class_0.__parent.__init(self, x, y, max_dx, max_dy, acc)
+      self.dy = dy
+      self.r = r
+      self.col = col
+    end,
+    __base = _base_0,
+    __name = "Bullet",
+    __parent = _parent_0
+  }, {
+    __index = function(cls, name)
+      local val = rawget(_base_0, name)
+      if val == nil then
+        local parent = rawget(cls, "__parent")
+        if parent then
+          return parent[name]
+        end
+      else
+        return val
+      end
+    end,
+    __call = function(cls, ...)
+      local _self_0 = setmetatable({}, _base_0)
+      cls.__init(_self_0, ...)
+      return _self_0
+    end
+  })
+  _base_0.__class = _class_0
+  if _parent_0.__inherited then
+    _parent_0.__inherited(_parent_0, _class_0)
+  end
+  Bullet = _class_0
+end
+local bullets = { }
 local friction = 0.90
-local horizon = (s_hei - 48) / 2
 local terrain_lines = { }
 local terrain_lines_acc = 0.05
 local ease_in_quad
@@ -45,7 +134,7 @@ ease_out_quad = function(t)
 end
 local terrain_lines_upd
 terrain_lines_upd = function()
-  if t % math.floor(15 - (terrain_lines_acc * 50)) == 0 then
+  if (t % (math.ceil(1 / terrain_lines_acc) * 2) == 0) and (terrain_lines_acc ~= 0) then
     table.insert(terrain_lines, {
       y = horizon,
       dy = 0
@@ -54,7 +143,7 @@ terrain_lines_upd = function()
   if #terrain_lines > 0 then
     for lin = #terrain_lines, 1, -1 do
       local t_lin = terrain_lines[lin]
-      local ease_lin = ease_in_quad((s_hei - t_lin.y) / (s_hei - horizon))
+      local ease_lin = ease_in_quad((s_hei - t_lin.y) / (s_hei - horizon)) / 2
       t_lin.dy = t_lin.dy + (terrain_lines_acc * ease_lin)
       t_lin.y = t_lin.y + t_lin.dy
       if t_lin.y >= s_hei then
@@ -87,16 +176,16 @@ terrain_mtns_draw = function()
   end
 end
 terrain_mtns_generate()
-local ship = Entity(60, 116, 3, 2, 0.4)
+local ship = Entity(60, 104, 3, 2, 0.4)
 ship.update = function(self)
   if btn(0) then
-    if (terrain_lines_acc < 0.07) then
-      terrain_lines_acc = terrain_lines_acc + 0.01
+    if (terrain_lines_acc < 0.12) then
+      terrain_lines_acc = terrain_lines_acc + 0.0025
     end
   end
   if btn(1) then
-    if (terrain_lines_acc > 0.03) then
-      terrain_lines_acc = terrain_lines_acc - 0.01
+    if (terrain_lines_acc >= 0.02) then
+      terrain_lines_acc = terrain_lines_acc - 0.0025
     end
   end
   if btn(2) then
@@ -113,25 +202,47 @@ ship.update = function(self)
     self.turn_r = false
     self.turn_l = false
   end
+  if btnp(4) then
+    table.insert(bullets, Bullet(self.x, self.y, 0.05, 2))
+  end
   self.dx = self.dx * friction
   self.x = self.x + self.dx
 end
 ship.draw = function(self)
+  local acc_mod = 0
   if self.turn_l then
-    return tri(self.x - 2, self.y, self.x - 4, self.y + 8, self.x + 4, self.y + 4, 14)
+    tri(self.x - 2, self.y + 2 + acc_mod, self.x - 4, self.y + 10 + acc_mod, self.x + 5, self.y + 7 + acc_mod, 1)
+    tri(self.x - 2, self.y, self.x - 4, self.y + 7, self.x + 4, self.y + 4, 11)
+    return tri(self.x - 1, self.y + 1, self.x - 4, self.y + 7, self.x + 4, self.y + 4, 15)
   elseif self.turn_r then
-    return tri(self.x + 2, self.y, self.x - 4, self.y + 4, self.x + 4, self.y + 8, 14)
+    tri(self.x + 2, self.y + 2 + acc_mod, self.x - 5, self.y + 7 + acc_mod, self.x + 4, self.y + 10 + acc_mod, 1)
+    tri(self.x + 2, self.y, self.x - 4, self.y + 4, self.x + 4, self.y + 7, 11)
+    return tri(self.x + 2, self.y, self.x - 4, self.y + 3, self.x + 3, self.y + 7, 15)
   else
-    return tri(self.x, self.y + 2, self.x - 4, self.y + 8, self.x + 4, self.y + 8, 14)
+    tri(self.x, self.y + 4 + acc_mod, self.x - 4, self.y + 11 + acc_mod, self.x + 4, self.y + 11 + acc_mod, 1)
+    tri(self.x, self.y + 2, self.x - 4, self.y + 8, self.x + 4, self.y + 8, 15)
+    return rect(self.x - 4, self.y + 9, 9, 1, 11)
   end
 end
-TIC = function()
-  cls(2)
+local planet_flight_update
+planet_flight_update = function()
+  cls(4)
   rect(0, horizon, s_wid, (s_hei + 48) / 2, 13)
   rect(0, horizon, s_wid, 1, 0)
   terrain_mtns_draw()
   terrain_lines_upd()
+  if #bullets > 0 then
+    for b = #bullets, 1, -1 do
+      bullets[b]:upd()
+      if not bullets[b].alive then
+        table.remove(bullets, b)
+      end
+    end
+  end
   ship:update()
-  ship:draw()
+  return ship:draw()
+end
+TIC = function()
+  planet_flight_update()
   t = t + 1
 end
--- game.moon
+++ game.moon
@@ -8,9 +8,12 @@ t=0
 s_wid = 240
 s_hei = 136
 
+-- land flight
+horizon = (s_hei-48)/2
+
 -- classes
 class Entity
-	new: (x, y, max_dx, max_dy, acc) =>
+	new: (x, y, max_dx=3, max_dy=2, acc) =>
 		@x = x
 		@y = y
 		@dx = 0
@@ -18,14 +21,40 @@ class Entity
 		@acc = acc
 		@max_dx = max_dx
 		@max_dy = max_dy
+		@alive = true
 		@turn_l = false
 		@turn_r = false
 
+class Bullet extends Entity
+	new: (x, y, acc, r, col=3, dy=-1, max_dx=0, max_dy=2) =>
+		super x,y,max_dx,max_dy,acc
+		@dy = dy
+		@r = r
+		@col = col
+
+	upd: =>
+		trace(@dy)
+		if math.abs(@dy) < math.abs(@max_dy)
+			if @dy < 0 then @dy -= @acc
+			if @dy > 0 then @dy += @acc
+
+		@y += @dy
+
+		if @y < horizon+40 then @r=1
+		if @y < horizon+16 then @r=0
+
+		if @y < horizon - 4 or @y > s_hei
+			@alive = false
+		
+		else
+			if @r==0 then pix(@x, @y, @col)
+			else circ(@x, @y, @r, @col)
+
+-- bullets
+bullets = {}
+
 -- flight section
 friction = 0.90
-
--- land flight
-horizon = (s_hei-48)/2
 
 terrain_lines = {}
 
@@ -69,7 +98,7 @@ terrain_mtns_draw = ->
 terrain_mtns_generate!
 
 -- ship
-ship = Entity 60,116,3,2,0.4
+ship = Entity 60,104,3,2,0.4
 
 ship.update = =>
 	if btn 0
@@ -87,6 +116,9 @@ ship.update = =>
 	if (not btn 2) and (not btn 3)
 		@turn_r = false
 		@turn_l = false
+
+	if btnp 4
+		table.insert(bullets, Bullet(@x, @y, 0.05, 2))
 	
 	@dx *= friction
 	@x += @dx
@@ -96,29 +128,40 @@ ship.draw = =>
 	acc_mod = 0
 
 	if @turn_l
-		tri(@x-2, @y+2+acc_mod, @x-4, @y+10+acc_mod, @x+4, @y+6+acc_mod, 1) -- shadow
-		tri(@x-2, @y, @x-4, @y+8, @x+4, @y+4, 15) -- ship
+		tri(@x-2, @y+2+acc_mod, @x-4, @y+10+acc_mod, @x+5, @y+7+acc_mod, 1) -- shadow
+		tri(@x-2, @y, @x-4, @y+7, @x+4, @y+4, 11) -- ship
+		tri(@x-1, @y+1, @x-4, @y+7, @x+4, @y+4, 15) -- ship detail
 	elseif @turn_r
-		tri(@x+2, @y+2+acc_mod, @x-4, @y+6+acc_mod, @x+4, @y+10+acc_mod, 1) -- shadow
-		tri(@x+2, @y, @x-4, @y+4, @x+4, @y+8, 15) -- ship
+		tri(@x+2, @y+2+acc_mod, @x-5, @y+7+acc_mod, @x+4, @y+10+acc_mod, 1) -- shadow
+		tri(@x+2, @y, @x-4, @y+4, @x+4, @y+7, 11) -- ship
+		tri(@x+2, @y, @x-4, @y+3, @x+3, @y+7, 15) -- ship detail
 	else
-		tri(@x, @y+4+acc_mod, @x-4, @y+10+acc_mod, @x+4, @y+10+acc_mod, 1) -- shadow
+		tri(@x, @y+4+acc_mod, @x-4, @y+11+acc_mod, @x+4, @y+11+acc_mod, 1) -- shadow
 		tri(@x, @y+2, @x-4, @y+8, @x+4, @y+8, 15) -- ship
+		rect(@x-4, @y+9, 9, 1, 11)
 
 
-export TIC=->
+planet_flight_update = ->
 	cls 4
 
 	rect 0, horizon, s_wid, (s_hei+48)/2, 13
 	rect 0, horizon, s_wid, 1, 0
 
 	terrain_mtns_draw!
-	
 	terrain_lines_upd!
+
+	if #bullets > 0
+		for b=#bullets, 1, -1
+			bullets[b]\upd!
+			if not bullets[b].alive
+				table.remove(bullets, b)
 
 	ship\update!
 	ship\draw!
-	
+
+
+export TIC=->
+	planet_flight_update!
 	t+=1
 	
 -- db16: 140c1c44243430346d4e4a4e854c30346524d04648757161597dced27d2c8595a16daa2cd2aa996dc2cadad45edeeed6
